16cbc81d28ad1372f1c8d7e4d1447762e119c00e
ngapham <ngapt.fit@gmail.com>
2015-07-28 21:37:07 +0200

    update control

--- src/com/pulsardev/homebudgettracker/control/JSONSerializer.java
+++ src/com/pulsardev/homebudgettracker/control/JSONSerializer.java
@@ -5,33 +5,50 @@
  */
 package com.pulsardev.homebudgettracker.control;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import com.pulsardev.homebudgettracker.model.ExpenseCategory;
+import com.pulsardev.homebudgettracker.model.ExpenseDateReport;
 
 import android.content.Context;
 
 public class JSONSerializer {
 	
 	private Context mContext;
-	private String mFilename;
+	private String mFileName;
 	
 	/**
 	 * constructor from specific context and filename
 	 */
-	public JSONSerializer(Context mContext, String mFilename) {
+	public JSONSerializer(Context mContext, String mFileName) {
 		this.mContext = mContext;
-		this.mFilename = mFilename;
+		this.mFileName = mFileName;
 	}
 	
-	public void saveExpenseCategory(ArrayList<ExpenseCategory> exCatList) throws JSONException {
+	public void saveListExpenseDateReport(ArrayList<ExpenseDateReport> exDateReportList) throws JSONException, IOException {
 		// Build an array in JSON
 		JSONArray mArray = new JSONArray();
-		for (ExpenseCategory item : exCatList) {
+		for (ExpenseDateReport item : exDateReportList) {
 			mArray.put(item.toJSON());
+		}
+		// Write the file to disk
+		Writer writer = null;
+		try {
+			OutputStream out = mContext.openFileOutput(mFileName, Context.MODE_PRIVATE);
+			writer = new OutputStreamWriter(out);
+			writer.write(mArray.toString());
+		} finally {
+			if (writer != null) {
+				writer.close();
+			}
 		}
 	}
 }
